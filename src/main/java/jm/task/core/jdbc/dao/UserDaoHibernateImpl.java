e51496c6ca8bbd0416bcd29163ff245794b12eae
dmtch-github <dmtch@yandex.ru>
2021-11-11 17:40:39 +0300

    Сделал заготовку для реализации Hibernate

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,11 +1,17 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 public class UserDaoHibernateImpl implements UserDao {
+    private final Logger log = Logger.getLogger(Class.class.getName());
+    private final Util util;
+
     public UserDaoHibernateImpl() {
+        util = new Util();
 
     }
 
